0f6a1ff0276dbfc563b6b4019778c580787b18fb
magnus <magnuspicataggio@gmail.com>
2026-07-31 21:00:07 -0400

    Verified output to cout

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,22 @@
+#include <pthread/qos.h>
+
+#include <cassert>
 #include <iostream>
 
+#include "cache_algorithms/experiments.h"
 #include "system_info/system_info.h"
+
 using namespace std;
 int main() {
+    // Uses P-Cores
+    pthread_set_qos_class_self_np(QOS_CLASS_USER_INTERACTIVE, 0);
+    // Need for apple
     const AppleSystemInfo& info = AppleSystemInfo::get_instance();
-
     info.print_summary();
+    // assert(info.cache_line_size <= 128);
+    //
+    // std::vector v = cache_size_detection();
+    // display_measurements(v);
 
     return 0;
 }
